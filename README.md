3b65ad33c4ed4ca6e7b3cc7953f3dbc3a558b6d7
Roman Heinrich <roman.heinrich@gmail.com>
2017-04-19 15:43:42 +0200

    some testing...

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
 
 a small test for Ruby bindings for LMDB: https://github.com/minad/lmdb
 
-
     $ git clone https://github.com/mindreframer/kvtest
     $ cd kvtest
     $ bundle install
     $ bin/console
+
+    # this will create 100_000 small records
+    > DbWrapper.generate
+    > DbWrapper.show_stats
+    > DbWrapper.get("key_99999")
+    > DbWrapper.get("key_444")
+    > DbWrapper.get("key_1")
+    > DbWrapper.get("key_0")
+    => nil
--- lib/kvtest.rb
+++ lib/kvtest.rb
@@ -1,23 +1,52 @@
-require "kvtest/version"
 require "pry"
-
 require "lmdb"
-module Kvtest
-  def self.run
-    path   = "data"
-    env    = LMDB.new(path)
-    maindb = env.database
+require "kvtest/version"
 
-    maindb['key'] = 'value'
-    maindb['key3'] = 'value3'
-    maindb['some'] = "another value"
+class DbWrapper
+  def self.instance
+    @instance ||= env.database
+  end
 
-    maindb.cursor do |c|
-      while a = c.next
-        puts a
+  def self.env
+    @env ||= begin
+      env = LMDB.new("data", :mapsize => (1024 * 1024 * 512))
+      at_exit do
+        puts "closing db handle..."
+        close(env)
       end
+      env
     end
+  end
+
+  def self.close(e)
+    e.close
+  rescue LMDB::Error
+    puts "env was closed..."
+  end
+
+  def self.get(v)
+    instance[v]
+  end
+
+  def self.generate
+    i = 0
+    while i < 100000 do
+      instance["key_#{i}"] = "value_#{i}"
+      print "." if i % 1000 == 0
+      i = i + 1
+    end
+  end
+
+  def self.reset!
     env.close
+    @env      = nil
+    @instance = nil
+    GC.start
+  end
+
+  def self.show_stats
+    pid = Process.pid
+    rss = `ps -eo pid,rss | grep #{pid} | awk '{print $2}'`.to_i
+    puts rss
   end
 end
-Kvtest.run
